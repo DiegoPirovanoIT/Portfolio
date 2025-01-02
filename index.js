c75d11cff57af4feea0a9a864eeeacb9b53209b2
Diego Pirovano <139859822+CubettoCubetto@users.noreply.github.com>
2025-01-02 15:11:25 +0100

    Added language support

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
+var content_description = [];
+let removeDescriptionTimeOut;
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Add event listener for scrolling to the 'more-info' section
+    let buttons = document.getElementsByClassName("grid-item");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', (event) => goToMoreInfo(buttons[i].getAttribute("value")));
+    }
+
+    // Add event listener for the 'Back to Top' button
+    let backToTopButtons = document.getElementsByClassName("back-to-top");
+    for (let i = 0; i < backToTopButtons.length; i++) {
+        backToTopButtons[i].addEventListener('click', scrollToTop);
+    }
+    content_description = document.getElementsByClassName("description");
+});
+
+
 // Function to scroll to the 'more-info' section
-function scrollToContent() {
+function goToMoreInfo(value) {
+    document.getElementById("more-info").classList.remove("disabled");
+    clearTimeout(removeDescriptionTimeOut);
     document.getElementById('more-info').scrollIntoView({
         behavior: 'smooth'
     });
-    console.log("Scrolling to More Info section");
+    for (let i = 0; i < content_description.length; i++) {
+        const desc = content_description[i];
+        if (desc.classList.contains(value)) {
+            desc.classList.remove('disabled');
+        } else {
+            desc.classList.add('disabled');
+        }
+    }
 }
 
-// Function to scroll back to the top of the page or a specific section
+function removeDescription(){
+    document.getElementById("more-info").classList.add("disabled");
+    for (let i = 0; i < content_description.length; i++) {
+        const desc = content_description[i];
+        if (!desc.classList.contains("disabled")) {
+            desc.classList.add('disabled');
+        }
+    }
+}
+
 function scrollToTop() {
     document.querySelector('.top').scrollIntoView({
-        behavior: 'smooth' // Smooth scrolling back to the top
+        behavior: 'smooth'
     });
     console.log("Scrolling back to the top");
+
+    removeDescriptionTimeOut = setTimeout(removeDescription, 1000);
 }
 
-// Add event listener for scrolling to the 'more-info' section
-let buttons = document.getElementsByClassName("grid-item");
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', scrollToContent);
-}
 
-// Add event listener for the 'Back to Top' button
-document.getElementById('back-to-top').addEventListener('click', scrollToTop);
+
---
+++ language.js
@@ -0,0 +1,70 @@
+const boxes = document.querySelectorAll('.box'); // Target the parent boxes
+const testi_italiani = document.querySelectorAll(".it")
+const testi_inglesi = document.querySelectorAll(".en")
+let LANG_SEL = "it"; // Set the currently selected language
+
+boxes.forEach(box => {
+    box.addEventListener('click', () => {
+        // Get the value of the clicked box (the language code)
+        const selectedLanguage = box.getAttribute('value');
+
+        // Update the LANG_SEL variable
+        LANG_SEL = selectedLanguage;
+
+        cambiaLingua();
+        
+    });
+});
+
+
+function cambiaLingua(){
+    if(LANG_SEL == "it"){
+        testi_italiani.forEach(t => {
+            t.classList.remove("disabled")
+        });
+        testi_inglesi.forEach(t => {
+            t.classList.add("disabled")
+        });
+    }
+    else{
+        testi_italiani.forEach(t => {
+            t.classList.add("disabled")
+        });
+        testi_inglesi.forEach(t => {
+            t.classList.remove("disabled")
+        });
+    }
+}
+
+boxes.forEach(box => {
+    box.addEventListener('mouseenter', () => {
+        boxes.forEach(b => {
+            const flag = b.querySelector('.flag'); // Get the flag inside the box
+            if (b !== box) {
+                flag.classList.add('language-non-selected');
+                flag.classList.remove('language-selected');
+            } else {
+                flag.classList.add('language-selected');
+                flag.classList.remove('language-non-selected');
+            }
+        });
+    });
+
+    box.addEventListener('mouseleave', () => {
+        boxes.forEach(b => {
+            const flag = b.querySelector('.flag'); // Get the flag inside the box
+            if (flag.getAttribute('value') === LANG_SEL) {
+                flag.classList.remove('language-non-selected');
+                flag.classList.add('language-selected');
+            } else {
+                flag.classList.remove('language-selected');
+                flag.classList.add('language-non-selected');
+            }
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    cambiaLingua();
+});
